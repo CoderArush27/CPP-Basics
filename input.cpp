277dca98b7056960bc9fcd74482b80f25b445da8
CoderArush27 <arush.ag11@gmail.com>
2021-12-28 13:48:15 +0530

    Input

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"ENTER THE VALUE OF a"<<endl;
+    cin>>a;
+    cout<<"ENTER THE VALUE OF b"<<endl;
+    cin>>b;
+
+    cout<<"VALUE OF a "<<a<<endl;
+    cout<<"VALUE OF b "<<b<<endl;
+    return 0;
+}
