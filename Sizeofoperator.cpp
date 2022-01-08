928738dfaa19dc9037f70aa68ebc857541e79fba
CoderArush27 <arush.ag11@gmail.com>
2022-01-08 22:08:13 +0530

    SizeofOperator

---
+++ Sizeofoperator.cpp
@@ -0,0 +1,23 @@
+/*sizeof operator is used to calculate the amount of RAM occupied in the computer*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    bool b;
+    char c;
+    double d;
+    float f;
+    long int g;
+    short int h;
+    string i;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(g)<<endl;
+    cout<<sizeof(h)<<endl;
+    cout<<sizeof(i)<<endl;
+    
+return 0;
+}
