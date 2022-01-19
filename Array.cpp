46c678e3d9913ce1d5826aa5d55ef09653527baf
CoderArush27 <arush.ag11@gmail.com>
2022-01-19 13:28:02 +0530

    array

---
+++ Array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={10,20,30,40,50};
+    //traversal
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+return 0;    
+}
